e13961cbdcf0ff5e07f9d3f94b7bae40da6ebedf
JunoJunho <wnsgh611@gmail.com>
2016-12-27 15:28:16 +0900

    Chapter 3

---
+++ src/main/java/com/junojunho/AmazonProperties.java
@@ -0,0 +1,19 @@
+package com.junojunho;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("amazon")
+public class AmazonProperties {
+
+    private String associatedId;
+
+    public void setAssociatedId(String associatedId){
+        this.associatedId = associatedId;
+    }
+
+    public String getAssociatedId(){
+        return this.associatedId;
+    }
+}
